06b992c61d2cea0f9923407c331cb800c19017a5
drthvdr <jaredrodr2@gmail.com>
2015-01-22 02:31:28 -0500

    increase again

--- src/RaceCarDemo/RaceCar.java
+++ src/RaceCarDemo/RaceCar.java
@@ -5,7 +5,7 @@ public class RaceCar {
 	private String name;
 	private int location = 0;
 	
-	private static int maxSpeedForAll = 140;
+	private static int maxSpeedForAll = 150;
 	
 	public RaceCar( int speed, String name , int location) {
 		setSpeed(speed);
